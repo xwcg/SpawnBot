ca0a31fc80dfd8326c4b00188bd583cd87d89051
Michael Schwarz <michael@xware-cg.com>
2012-06-02 22:30:33 +0200

    Rudimentary working version of xIrcNet, the new and improved IRC Library for Spawnbot.
    NetTestApp is a simple console app to test xIrcNet.
    
    Signed-off-by: Michael Schwarz <michael@xware-cg.com>

---
+++ NetTestApp/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using xIrcNet;
+using xLogger;
+
+namespace NetTestApp
+{
+    class Program
+    {
+        static IRC test;
+
+        static void Main( string[] args )
+        {
+            test = new IRC("testbot", "irc.quakenet.org", 6667);
+
+            test.eventCommandReceived += new IrcCommandReceived(test_eventCommandReceived);
+
+            test.eventConnected += new IrcConnected(test_eventConnected);
+            test.eventDisconnected += new IrcDisconnected(test_eventDisconnected);
+            test.eventConnectingError += new IrcConnectingError(test_eventConnectingError);
+            test.eventCommandSent += new IrcCommandSent(test_eventCommandSent);
+            test.eventRawBotModeReceived += new IrcRawBotModeGet(test_eventRawBotModeReceived);
+            test.eventUserModeChanged += new IrcUserMode(test_eventUserModeChanged);
+            test.eventMessageReceived += new IrcMessage(test_eventMessageReceived);
+            test.eventNameListReceived += new IrcNameListReceived(test_eventNameListReceived);
+            test.eventUserKicked += new IrcUserKicked(test_eventUserKicked);
+
+            string cmd;
+
+            while ( ( cmd = Console.ReadLine() ) != null )
+            {
+                if ( cmd.StartsWith("say") )
+                {
+                    string[] parts = cmd.Split(' ');
+
+                    string channel = parts[1];
+                    string message = cmd.Substring(cmd.IndexOf(channel) + channel.Length + 1);
+
+                    test.SendCommand(String.Format("PRIVMSG {0} :{1}", channel, message));
+                    continue;
+                }
+
+                switch ( cmd.ToLower() )
+                {
+                    case "quit":
+                        test.Disconnect();
+                        Environment.Exit(0);
+                        break;
+                    case "connect":
+                        test.Connect();
+                        break;
+                    case "disconnect":
+                        test.Disconnect();
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command '" + cmd + "'");
+                        break;
+                }
+            }
+        }
+
+        static void test_eventUserKicked( string channel, string name, string by, string message )
+        {
+            Logger.WriteLine(name + " was kicked by " + by + " in " + channel + " for " + message, ConsoleColor.Yellow);
+        }
+
+        static void test_eventNameListReceived( string channel, string[] list )
+        {
+            Logger.WriteLine("USER LIST FOR " + channel, ConsoleColor.Blue);
+            foreach ( string name in list )
+            {
+                Logger.WriteLine(name, ConsoleColor.DarkBlue);
+            }
+            Logger.WriteLine("END OF LIST");
+        }
+
+        static void test_eventMessageReceived( string channel, string name, string message )
+        {
+            Logger.WriteLine(String.Format("({0}) {1}: {2}", channel, name, message), ConsoleColor.White);
+        }
+
+        static void test_eventUserModeChanged( string channel, string name, string mode, string by )
+        {
+            Logger.WriteLine(String.Format("({0}) MODE {1} TO {2} BY {3}", new object[] { channel, mode, name, by }), ConsoleColor.Yellow);
+        }
+
+        static void test_eventRawBotModeReceived( string mode )
+        {
+            if ( mode == "+i" )
+            {
+                test.SendCommand("JOIN #thepretz");
+            }
+        }
+
+        static void test_eventRawMotdLineReceived( string line )
+        {
+            Logger.WriteLine("MOTD: " + line, ConsoleColor.Yellow);
+        }
+
+        static void test_eventCommandReceived( string cmd )
+        {
+            Logger.WriteLine("<- " + cmd, ConsoleColor.DarkMagenta);
+        }
+
+        static void test_eventConnectingError( string error )
+        {
+            Logger.WriteLine("Connecting Error! : " + error, ConsoleColor.Red);
+        }
+
+        static void test_eventCommandSent( string cmd )
+        {
+            Logger.WriteLine("-> " + cmd, ConsoleColor.DarkCyan);
+        }
+
+        static void test_eventConnected()
+        {
+            Logger.WriteLine("Connected!", ConsoleColor.Green);
+        }
+
+        static void test_eventDisconnected( string msg )
+        {
+            Logger.WriteLine("Disconnected!", ConsoleColor.Green);
+        }
+    }
+}
---
+++ xIrcNet/Main.cs
@@ -0,0 +1,627 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace xIrcNet
+{
+    #region Delegates
+
+    public delegate void IrcCommandSent( string cmd );
+    public delegate void IrcCommandReceived( string cmd );
+    public delegate void IrcServerMessageReceived( string cmd );
+    public delegate void IrcServerPingReceived( string hash );
+    public delegate void IrcMessage( string channel, string name, string message );
+
+    public delegate void IrcTopicReceived( string channel, string topic );
+    public delegate void IrcTopicOwnerRecevied( string channel, string name, string date );
+    public delegate void IrcNameListReceived( string channel, string[] list );
+
+    public delegate void IrcUserJoin( string channel, string name );
+    public delegate void IrcUserPart( string channel, string name );
+    public delegate void IrcUserMode( string channel, string name, string mode, string by );
+    public delegate void IrcUserChange( string name, string newName );
+    public delegate void IrcUserKicked( string channel, string name, string by, string message );
+    public delegate void IrcUserQuit( string name, string message );
+
+    public delegate void IrcConnected();
+    public delegate void IrcDisconnected( string msg );
+    public delegate void IrcConnectingError( string error );
+
+    public delegate void IrcRawMotdStart();
+    public delegate void IrcRawMotdLineGet( string line );
+    public delegate void IrcRawMotdEnd();
+    public delegate void IrcRawBotModeGet( string mode );
+    #endregion
+
+    public class IRC
+    {
+        #region Events
+
+        public event IrcUserJoin eventUserJoined;
+        public event IrcUserKicked eventUserKicked;
+        public event IrcUserMode eventUserModeChanged;
+        public event IrcUserPart eventUserPart;
+        public event IrcUserQuit eventUserQuit;
+        public event IrcUserChange eventUserChangedNick;
+
+        public event IrcCommandReceived eventCommandReceived;
+        public event IrcCommandSent eventCommandSent;
+        public event IrcMessage eventMessageReceived;
+        public event IrcServerMessageReceived eventServerMessageReceived;
+        public event IrcServerPingReceived eventServerPingReceived;
+
+        public event IrcTopicReceived eventTopicReceived;
+        public event IrcTopicOwnerRecevied eventTopicOwnerReceived;
+
+        public event IrcNameListReceived eventNameListReceived;
+
+        public event IrcConnected eventConnected;
+        public event IrcDisconnected eventDisconnected;
+        public event IrcConnectingError eventConnectingError;
+
+        #endregion
+
+        #region RawEvents
+
+        public event IrcRawMotdStart eventRawMotdStarted;
+        public event IrcRawMotdLineGet eventRawMotdLineReceived;
+        public event IrcRawMotdEnd eventRawMotdEnded;
+
+        public event IrcRawBotModeGet eventRawBotModeReceived;
+
+        #endregion
+
+        #region Private
+
+        private string _Server;
+        private int _Port;
+        private string _Nick;
+        private string _User;
+        private string _RealName;
+        private TcpClient _Connection;
+        private NetworkStream _Stream;
+        private StreamWriter _Out;
+        private StreamReader _In;
+
+        private Thread _ReadThread;
+        private string _Host;
+        #endregion
+
+        #region Properties
+        public string Server
+        {
+            get
+            {
+                return _Server;
+            }
+        }
+
+        public int Port
+        {
+            get
+            {
+                return _Port;
+            }
+        }
+
+        public string Nick
+        {
+            get
+            {
+                return _Nick;
+            }
+            set
+            {
+                _Nick = value;
+            }
+        }
+
+        public string User
+        {
+            get
+            {
+                return _User;
+            }
+            set
+            {
+                _User = value;
+            }
+        }
+
+        public string RealName
+        {
+            get
+            {
+                return _RealName;
+            }
+            set
+            {
+                _RealName = value;
+            }
+        }
+
+        public TcpClient Connection
+        {
+            get
+            {
+                return _Connection;
+            }
+        }
+
+        public NetworkStream Stream
+        {
+            get
+            {
+                return _Stream;
+            }
+        }
+
+        public StreamWriter StreamOut
+        {
+            get
+            {
+                return _Out;
+            }
+        }
+
+        public StreamReader StreamIn
+        {
+            get
+            {
+                return _In;
+            }
+        }
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new IRC Connection
+        /// </summary>
+        /// <param name="name">Nickname/Handle</param>
+        /// <param name="server">The server to connect to</param>
+        /// <param name="port">The port where to connect to</param>
+        public IRC( string name, string server, int port )
+        {
+            _Nick = name;
+            _Server = server;
+            _Port = port;
+
+            _User = "Arthur";
+            _RealName = "Arthur Dent";
+
+            eventConnected += new IrcConnected(IRC_eventConnected);
+        }
+
+        public IRC( string name, string user, string realname, string server, int port )
+        {
+            _Nick = name;
+            _User = user;
+            _RealName = realname;
+            _Server = server;
+            _Port = port;
+
+            eventConnected += new IrcConnected(IRC_eventConnected);
+        }
+
+        void IRC_eventConnected()
+        {
+            SendCommand(String.Format("USER {0} 8 * :{1}", _User, _RealName));
+            SendCommand(String.Format("NICK {0}", _Nick));
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Disconnect()
+        {
+            Disconnect("So long and thanks for all the fish");
+        }
+
+        public void Disconnect( string msg )
+        {
+            if ( _Connection != null && _Connection.Connected )
+            {
+                if ( _ReadThread.ThreadState == ThreadState.Running || _ReadThread.ThreadState == ThreadState.Background )
+                {
+                    _ReadThread.Abort();
+                }
+
+                SendCommand(String.Format("QUIT :{0}", msg));
+
+                _In.Close();
+                _Out.Close();
+                _Stream.Close();
+                _Connection.Close();
+
+                _In.Dispose();
+                _Out.Dispose();
+                _Stream.Dispose();
+                _Connection = null;
+
+                if ( eventDisconnected != null )
+                {
+                    eventDisconnected(msg);
+                }
+            }
+        }
+
+        public void Connect()
+        {
+            if ( _Connection != null && _Connection.Connected == true )
+            {
+                if ( eventConnectingError != null )
+                {
+                    eventConnectingError("Already connected.");
+                }
+
+                return;
+            }
+
+            try
+            {
+                _Connection = new TcpClient(_Server, _Port);
+                _Stream = _Connection.GetStream();
+                _In = new StreamReader(_Stream);
+                _Out = new StreamWriter(_Stream);
+
+                if ( _Connection.Connected )
+                {
+                    if ( eventConnected != null )
+                    {
+                        eventConnected();
+                    }
+
+                    _ReadThread = new Thread(new ThreadStart(ReceiveLoop));
+                    _ReadThread.IsBackground = true;
+                    _ReadThread.Start();
+                }
+                else
+                {
+                    if ( eventConnectingError != null )
+                    {
+                        eventConnectingError("Tcp Client could not connect");
+                    }
+                }
+            }
+            catch ( Exception e )
+            {
+                if ( eventConnectingError != null )
+                {
+                    eventConnectingError(e.Message);
+                }
+            }
+        }
+
+        public void SendCommand( string command )
+        {
+            if ( _Out != null && _Connection.Connected )
+            {
+                _Out.WriteLine(command);
+                _Out.Flush();
+
+                if ( eventCommandSent != null )
+                {
+                    eventCommandSent(command);
+                }
+            }
+        }
+        #endregion
+
+        #region Input Handling
+
+        private void ReceiveLoop()
+        {
+            while ( _In != null && _Connection.Connected )
+            {
+                string RawIn;
+
+                if ( ( RawIn = _In.ReadLine() ) != null )
+                {
+                    if ( eventCommandReceived != null )
+                    {
+                        eventCommandReceived(RawIn);
+                    }
+
+                    string[] RawParts = RawIn.Split(' ');
+
+                    if ( RawParts[0].StartsWith(":") )
+                    {
+                        RawParts[0] = RawParts[0].Substring(1);
+                    }
+
+                    switch ( RawParts[0] )
+                    {
+                        case "PING":
+                            HandlePing(RawParts[1]);
+                            break;
+                    }
+
+                    switch ( RawParts[1] )
+                    {
+                        // WELCOME
+                        case "001":
+                            _Host = RawParts[0];
+                            break;
+
+                        // YOUR HOST
+                        case "002":
+                            break;
+
+                        // CREATED
+                        case "003":
+                            break;
+
+                        // MY INFO
+                        case "004":
+                            break;
+
+                        // I SUPPORT
+                        case "005":
+                            break;
+
+                        // U(R) MODE IS
+                        case "221":
+                            if ( eventRawBotModeReceived != null )
+                            {
+                                eventRawBotModeReceived(RawParts[3]);
+                            }
+                            break;
+
+                        // TOPIC
+                        case "332":
+                            HandleTopic(RawGetOnlyText(RawIn));
+                            break;
+
+                        // TOPIC WHO TIME
+                        case "333":
+                            HandleTopicInfo(RawGetOnlyText(RawIn));
+                            break;
+
+                        // NAME REPLY
+                        case "353":
+                            HandleNameList(RawGetOnlyText(RawIn));
+                            break;
+
+                        // END OF NAMES
+                        case "366":
+                            break;
+
+                        // MOTD START
+                        case "375":
+                            if ( eventRawMotdStarted != null )
+                            {
+                                eventRawMotdStarted();
+                            }
+                            break;
+
+                        // MOTD
+                        case "372":
+                            if ( eventRawMotdLineReceived != null )
+                            {
+                                eventRawMotdLineReceived(RawGetOnlyText(RawIn));
+                            }
+                            break;
+
+                        // MOTD END
+                        case "376":
+                            if ( eventRawMotdEnded != null )
+                            {
+                                eventRawMotdEnded();
+                            }
+                            break;
+
+                        ////// Normal command
+
+                        case "MODE":
+                            HandleMode(RawParts);
+                            break;
+
+                        case "PRIVMSG":
+                            HandlePrivMsg(RawGetSourceNick(RawParts[0]), RawIn);
+                            break;
+
+                        case "NICK":
+                            HandleNick(RawGetSourceNick(RawParts[0]), RawParts[2]);
+                            break;
+
+                        case "KICK":
+                            HandleKick(RawParts);
+                            break;
+
+                        case "JOIN":
+                            if ( eventUserJoined != null )
+                            {
+                                eventUserJoined(RawParts[2], RawGetSourceNick(RawParts[0]));
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Cleans a raw command from all junk and returns only the text.
+        /// Example: :irc.server.com 333 botname #channel user 123 -> #channel user 123
+        /// </summary>
+        /// <param name="rawcmd">Raw command to clean</param>
+        /// <returns>Only the command text</returns>
+        private string RawGetOnlyText( string rawcmd )
+        {
+            string text = rawcmd.Substring(rawcmd.IndexOf(_Nick) + _Nick.Length + 1);
+            if ( text.StartsWith(":") )
+            {
+                text = text.Substring(1);
+            }
+
+            return text;
+        }
+
+        private string RawGetSourceNick( string rawpart )
+        {
+            if ( rawpart.Contains("!") )
+            {
+                return rawpart.Substring(0, rawpart.IndexOf("!"));
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void HandlePing( string rawHash )
+        {
+            string hash = rawHash.Substring(1);
+
+            SendCommand(String.Format("PONG :{0}", hash));
+
+            if ( eventServerPingReceived != null )
+            {
+                eventServerPingReceived(hash);
+            }
+        }
+
+        private void HandleTopic( string CleanText )
+        {
+            string channel = CleanText.Substring(0, CleanText.IndexOf(" :"));
+            string topic = CleanText.Substring(CleanText.IndexOf(":") + 1);
+
+            if ( eventTopicReceived != null )
+            {
+                eventTopicReceived(channel, topic);
+            }
+        }
+
+        private void HandleTopicInfo( string CleanText )
+        {
+            string[] Parts = CleanText.Split(' ');
+
+            if ( eventTopicOwnerReceived != null )
+            {
+                eventTopicOwnerReceived(Parts[0], Parts[1], Parts[2]);
+            }
+        }
+
+        private void HandleNameList( string CleanText )
+        {
+            string[] Parts = CleanText.Split(' ');
+            string channel = Parts[1];
+            List<string> names = new List<string>();
+
+            for ( int i = 2; i < Parts.Length; i++ )
+            {
+                string name = Parts[i];
+
+                if ( name.StartsWith(":") )
+                {
+                    name = name.Substring(1);
+                }
+
+                names.Add(name);
+            }
+
+            if ( eventNameListReceived != null )
+            {
+                eventNameListReceived(channel, names.ToArray());
+            }
+        }
+
+        private void HandleMode( string[] rawparts )
+        {
+            if ( rawparts[2].StartsWith("#") )
+            {
+                string by = RawGetSourceNick(rawparts[0]);
+                string channel = rawparts[2];
+
+                string modeprefix = rawparts[3].Substring(0, 1);
+
+                char[] modes = rawparts[3].Substring(1).ToCharArray();
+
+                for ( int i = 0; i < modes.Length; i++ )
+                {
+                    if ( eventUserModeChanged != null )
+                    {
+                        eventUserModeChanged(channel, rawparts[i + 4], modeprefix + modes[i], by);
+                    }
+                }
+            }
+            else
+            {
+                string by = RawGetSourceNick(rawparts[0]);
+                string channel = rawparts[2];
+
+                if ( eventUserModeChanged != null )
+                {
+                    eventUserModeChanged(channel, by, rawparts[3], by);
+                }
+            }
+
+        }
+
+        private void HandlePrivMsg( string CleanSourceNick, string rawcmd )
+        {
+            string[] parts = rawcmd.Split(' ');
+
+            string name = CleanSourceNick;
+            string channel = parts[2];
+            string message = rawcmd.Substring(rawcmd.IndexOf(channel) + channel.Length + 2);
+
+            if ( eventMessageReceived != null )
+            {
+                eventMessageReceived(channel, name, message);
+            }
+        }
+
+        private void HandleNick( string CleanSourceNick, string NewNick )
+        {
+            string nick = NewNick;
+            if ( nick.StartsWith(":") )
+            {
+                nick = nick.Substring(1);
+            }
+
+            if ( eventUserChangedNick != null )
+            {
+                eventUserChangedNick(CleanSourceNick, NewNick);
+            }
+        }
+
+        private void HandleKick( string[] rawparts )
+        {
+            string by = RawGetSourceNick(rawparts[0]);
+            string channel = rawparts[2];
+            string name = rawparts[3];
+
+            string message = "";
+
+            for ( int i = 4; i < rawparts.Length; i++ )
+            {
+                if ( i == 4 )
+                {
+                    if ( rawparts[i].StartsWith(":") )
+                    {
+                        message = message + rawparts[i].Substring(1) + " ";
+                    }
+                }
+                else
+                {
+                    message = message + rawparts[i] + " ";
+                }
+            }
+
+            message = message.Trim();
+
+            if ( eventUserKicked != null )
+            {
+                eventUserKicked(channel, name, by, message);
+            }
+        }
+
+        #endregion
+    }
+}
