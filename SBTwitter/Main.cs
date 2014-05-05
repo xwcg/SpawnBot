f74d8f7b086c21d04349de1c562241e1c118602e
Michael Schwarz <x-w-a-r-e@gmx.net>
2014-05-05 20:40:20 +0200

    SBTwitter Update + More Functionality
    * Twitter link parsing fixed (Twitterizer -> Tweetvini)
    + !tweet [message] to tweet from within IRC for social fun
    + !tweet[dat|last|that] to tweet the last line said for social fun
    + Twitter messages addressed to the Bot account will be posted to
    channel if hashtag with channel name is present
    + !twitter follow|unfollow to stream tweets from a user to a channel,
    for example for fan channels to keep everybody in the loop on the latest
    tweets

--- SBTwitter/Main.cs
+++ SBTwitter/Main.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using ConfigManager;
 using SBPluginInterface;
-using Twitterizer;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using Tweetinvi;
 using xLogger;
-using ConfigManager;
 
 /*
     Copyright 2012-2013 Michael Schwarz
-  
+
     This file is part of SpawnBot.
 
     SpawnBot is free software: you can redistribute it and/or modify
@@ -30,44 +30,21 @@ namespace SBTwitter
 {
     public class Main : SBPlugin
     {
-        private SBPluginHost Host;
-
-        private string AccessToken;
         private string AccessSecret;
+        private string AccessToken;
         private string ConsumerKey;
         private string ConsumerSecret;
+        private Dictionary<string, List<string>> follows = new Dictionary<string, List<string>>();
+        private SBPluginHost Host;
+        private Dictionary<string, string> lastSaid = new Dictionary<string, string>();
 
         #region SBPlugin Members
-
-        public string PluginName
-        {
-            get
-            {
-                return "Twitter Message Parser";
-            }
-        }
-
-        public string Version
-        {
-            get
-            {
-                return "1.0";
-            }
-        }
 
         public string Author
         {
             get
             {
                 return "Michael Schwarz";
-            }
-        }
-
-        public string Website
-        {
-            get
-            {
-                return "";
             }
         }
 
@@ -87,35 +64,70 @@ namespace SBTwitter
 
                 List<Config> Cfgs = Host.PluginConfigManager.Load("sbtwitter", "keys.cfg");
 
-                if ( Cfgs == null )
+                if (Cfgs == null)
                 {
                     Logger.WriteLine("****** (SBTwitter) KEYS.CFG not found, Disabling. ******", ConsoleColor.Red);
                 }
                 else
                 {
-                    foreach ( Config c in Cfgs )
+                    foreach (Config c in Cfgs)
                     {
-                        switch ( c.Index )
+                        switch (c.Index)
                         {
                             case "accesstoken":
                                 AccessToken = c.Value;
                                 break;
+
                             case "accesssecret":
                                 AccessSecret = c.Value;
                                 break;
+
                             case "consumerkey":
                                 ConsumerKey = c.Value;
                                 break;
+
                             case "consumersecret":
                                 ConsumerSecret = c.Value;
                                 break;
                         }
                     }
 
+                    Tweetinvi.TwitterCredentials.SetCredentials(AccessToken, AccessSecret, ConsumerKey, ConsumerSecret);
                     Host.eventPluginChannelMessageReceived += new ChannelMessage(Host_eventPluginChannelMessageReceived);
+                    Host.eventPluginChannelCommandReceived += Host_eventPluginChannelCommandReceived;
+
+                    var twitterStream = Stream.CreateFilteredStream();
+                    twitterStream.AddTrack("HerobrineIRC");
+                    twitterStream.MatchingTweetReceived += twitterStream_MatchingTweetReceived;
+                    twitterStream.StartStreamMatchingAllConditionsAsync();
+
+                    LoadConfig();
+                    InitFollow();
                 }
+            }
+        }
 
+        public string PluginName
+        {
+            get
+            {
+                return "Twitter Message Parser";
+            }
+        }
 
+        public string Version
+        {
+            get
+            {
+                return "1.0";
+            }
+        }
+
+        public string Website
+        {
+            get
+            {
+                return "";
             }
         }
 
@@ -124,16 +136,94 @@ namespace SBTwitter
             return;
         }
 
-        #endregion
-
-        void Host_eventPluginChannelMessageReceived( string name, string message, string channel )
+        private void twitterStream_MatchingTweetReceived(object sender, Tweetinvi.Core.Events.EventArguments.MatchedTweetReceivedEventArgs e)
         {
-            if ( message.Contains("twitter.com") && ( message.Contains("/status/") || message.Contains("/statuses/") ) )
+            string name = e.Tweet.Creator.ScreenName;
+            string text = WebUtility.HtmlDecode(e.Tweet.Text);
+
+            Regex hashMatch = new Regex(@"#\w+");
+
+            if (text.IndexOf("#") != -1)
+            {
+                MatchCollection matches = hashMatch.Matches(text);
+                if (matches.Count > 0)
+                {
+                    Host.PluginResponse(matches[0].Value, String.Format("@{0} tweets you: {1}", name, text));
+                }
+            }
+        }
+
+        #endregion SBPlugin Members
+
+        private void Host_eventPluginChannelCommandReceived(string name, string channel, string command, string[] parameters)
+        {
+            switch (command.ToLower())
+            {
+                case "twitter":
+                    if (parameters.Length > 0)
+                    {
+                        switch (parameters[0].ToLower())
+                        {
+                            case "follow":
+                                if (parameters.Length > 1)
+                                {
+                                    this.AddFollow(channel, parameters[1]);
+                                    this.InitFollow();
+                                    Host.PluginResponse(channel, String.Format("Now following @{0} on Twitter for you!", parameters[1]));
+                                }
+                                else
+                                {
+                                    Host.PluginResponse(channel, "Usage: !twitter follow [username]");
+                                }
+                                break;
+
+                            case "unfollow":
+                                if (parameters.Length > 1)
+                                {
+                                    this.DelFollow(channel, parameters[1]);
+                                    this.InitFollow();
+                                    Host.PluginResponse(channel, String.Format("Not following @{0} on Twitter for you anymore!", parameters[1]));
+                                }
+                                else
+                                {
+                                    Host.PluginResponse(channel, "Usage: !twitter follow [username]");
+                                }
+                                break;
+                        }
+                    }
+                    break;
+
+                case "tweet":
+                    string msg = String.Join(" ", parameters);
+                    var t = Tweetinvi.Tweet.PublishTweet(String.Format("<{0}@{1}> {2}", name, channel, msg));
+                    Host.PluginResponse(channel, "Tweet sent! https://twitter.com/HerobrineIRC/status/" + t.IdStr);
+                    break;
+
+                case "tweetthat":
+                case "tweetdat":
+                case "tweetlast":
+                    if (lastSaid.ContainsKey(channel))
+                    {
+                        var t2 = Tweetinvi.Tweet.PublishTweet(lastSaid[channel]);
+                        Host.PluginResponse(channel, "Tweeted last line! https://twitter.com/HerobrineIRC/status/" + t2.IdStr);
+                    }
+                    break;
+            }
+        }
+
+        private void Host_eventPluginChannelMessageReceived(string name, string message, string channel)
+        {
+            if (!lastSaid.ContainsKey(channel))
+                lastSaid.Add(channel, String.Format("<{0}@{1}> {2}", name, channel, message));
+            else
+                lastSaid[channel] = String.Format("<{0}@{1}> {2}", name, channel, message);
+
+            if (message.Contains("twitter.com") && (message.Contains("/status/") || message.Contains("/statuses/")))
             {
                 string[] Parameters = message.Split(' ');
-                foreach ( string p in Parameters )
+                foreach (string p in Parameters)
                 {
-                    if ( p.Contains("twitter.com") && ( p.Contains("/status/") || p.Contains("/statuses/") ) )
+                    if (p.Contains("twitter.com") && (p.Contains("/status/") || p.Contains("/statuses/")))
                     {
                         try
                         {
@@ -142,9 +232,9 @@ namespace SBTwitter
                             parse = parse.Substring(parse.IndexOf("/status/"));
                             parse = parse.Replace("/status/", "");
 
-                            string text = PollTwitterStatus(Convert.ToDecimal(parse));
+                            string text = PollTwitterStatus(parse);
 
-                            if ( text == null )
+                            if (text == null)
                             {
                                 return;
                             }
@@ -154,7 +244,7 @@ namespace SBTwitter
                                 break;
                             }
                         }
-                        catch ( Exception e )
+                        catch (Exception e)
                         {
                             Logger.WriteLine(String.Format("***** Twitter error. Parsing: {0} ({1})", p, e.Message), ConsoleColor.DarkRed);
                             return;
@@ -164,24 +254,134 @@ namespace SBTwitter
             }
         }
 
-        private string PollTwitterStatus( decimal StatusId )
+        private bool LoadConfig()
         {
-            OAuthTokens t = new OAuthTokens();
-            t.AccessToken = AccessToken;
-            t.AccessTokenSecret = AccessSecret;
-            t.ConsumerKey = ConsumerKey;
-            t.ConsumerSecret = ConsumerSecret;
-
-            TwitterResponse<TwitterStatus> status = TwitterStatus.Show(t, StatusId);
-
-            if ( status.Result == RequestResult.Success )
+            List<Config> cfgfollows = Host.PluginConfigManager.Load("sbtwitter", "follows.cfg");
+            if (cfgfollows == null)
+                return false;
+            follows.Clear();
+            foreach (Config f in cfgfollows)
             {
-                return String.Format("@{0} posted: {1}", status.ResponseObject.User.ScreenName, status.ResponseObject.Text);
+                string channel = f.Index;
+                string[] tweeters = f.Value.Split(';');
+
+                follows.Add(channel, new List<string>(tweeters));
+            }
+
+            return true;
+        }
+
+        private bool SaveConfig()
+        {
+            List<Config> cfgFollows = new List<Config>();
+            foreach (KeyValuePair<string, List<string>> kp in follows)
+            {
+                cfgFollows.Add(new Config(kp.Key, String.Join(";", kp.Value.ToArray())));
+            }
+
+            return Host.PluginConfigManager.Save(cfgFollows, "sbtwitter", "follows.cfg");
+        }
+
+        private bool AddFollow(string channel, string tweeter)
+        {
+            if (!this.follows.ContainsKey(channel))
+            {
+                List<string> newlist = new List<string>();
+                newlist.Add(tweeter);
+                this.follows.Add(channel, newlist);
             }
             else
             {
-                return null;
+                this.follows[channel].Add(tweeter);
             }
+
+            return SaveConfig();
+        }
+
+        private bool DelFollow(string channel, string tweeter)
+        {
+            if (this.follows.ContainsKey(channel))
+            {
+                if (this.follows[channel].Contains(tweeter))
+                {
+                    this.follows[channel].Remove(tweeter);
+                }
+            }
+
+            return SaveConfig();
+        }
+
+        private Tweetinvi.Core.Interfaces.Streaminvi.IFilteredStream stream = null;
+
+        private void InitFollow()
+        {
+            if (stream != null)
+            {
+                stream.StopStream();
+            }
+
+            stream = Stream.CreateFilteredStream();
+            stream.MatchingTweetReceived += stream_MatchingTweetReceived;
+            foreach (KeyValuePair<string, List<string>> kp in this.follows)
+            {
+                string channel = kp.Key;
+                List<string> tweeters = kp.Value;
+
+                foreach (string tweeter in tweeters)
+                {
+                    //stream.AddTrack(tweeter);
+                    stream.AddFollow(User.GetUserFromScreenName(tweeter));
+                }
+            }
+
+            stream.StartStreamMatchingAllConditionsAsync();
+        }
+
+        private void stream_MatchingTweetReceived(object sender, Tweetinvi.Core.Events.EventArguments.MatchedTweetReceivedEventArgs e)
+        {
+            Tweetinvi.Core.Interfaces.ITweet t = e.Tweet;
+
+            foreach (KeyValuePair<string, List<string>> kp in this.follows)
+            {
+                string channel = kp.Key;
+                List<string> tweeters = kp.Value;
+                foreach (string tweeter in tweeters)
+                {
+                    if (tweeter.ToLower() == t.Creator.ScreenName.ToLower())
+                    {
+                        Host.PluginResponse(channel, String.Format("@{0} just tweeted: {1}", t.Creator.ScreenName, WebUtility.HtmlDecode(t.Text)));
+                    }
+                }
+            }
+        }
+
+        private string PollTwitterStatus(string StatusId)
+        {
+            string returnString = "@{0}";
+            var tweet = Tweetinvi.Tweet.GetTweet(Convert.ToInt64(StatusId));
+
+            if (tweet.IsRetweet)
+                returnString += " retweeted: {1}";
+            else
+                returnString += " tweeted: {1}";
+
+            return String.Format(returnString, tweet.Creator.ScreenName, WebUtility.HtmlDecode(tweet.Text));
+            //OAuthTokens t = new OAuthTokens();
+            //t.AccessToken = AccessToken;
+            //t.AccessTokenSecret = AccessSecret;
+            //t.ConsumerKey = ConsumerKey;
+            //t.ConsumerSecret = ConsumerSecret;
+
+            //TwitterResponse<TwitterStatus> status = TwitterStatus.Show(t, StatusId);
+
+            //if ( status.Result == RequestResult.Success )
+            //{
+            //    return String.Format("@{0} posted: {1}", status.ResponseObject.User.ScreenName, status.ResponseObject.Text);
+            //}
+            //else
+            //{
+            //    return null;
+            //}
         }
     }
 }
