3776c662824f8f82647a25b91e2ef607e78a4680
Michael Schwarz <x-w-a-r-e@gmx.net>
2014-05-08 12:54:51 +0200

    Formatting

--- SBLinkCommands/Main.cs
+++ SBLinkCommands/Main.cs
@@ -63,180 +63,180 @@ namespace SBLinkCommands
                 Host = value;
 
                 LoadDictionary();
-                Host.eventPluginChannelCommandReceived += new ChannelCommand(Host_eventPluginChannelCommandReceived);
+                Host.eventPluginChannelCommandReceived += new ChannelCommand( Host_eventPluginChannelCommandReceived );
             }
         }
 
-        public void Dispose()
+        public void Dispose ()
         {
             return;
         }
 
         #endregion
 
-        private bool LoadDictionary()
+        private bool LoadDictionary ()
         {
-            List<Config> list = Host.PluginConfigManager.Load("sblinkcommands", "cmds.cfg");
+            List<Config> list = Host.PluginConfigManager.Load( "sblinkcommands", "cmds.cfg" );
 
             cmds.Clear();
 
-            if (list == null)
+            if ( list == null )
             {
                 return false;
             }
 
-            foreach (Config l in list)
+            foreach ( Config l in list )
             {
-                cmds.Add(l.Index, l.Value);
+                cmds.Add( l.Index, l.Value );
             }
 
-            list = Host.PluginConfigManager.Load("sblinkcommands", "urls.cfg");
+            list = Host.PluginConfigManager.Load( "sblinkcommands", "urls.cfg" );
 
             urls.Clear();
 
-            if (list == null)
+            if ( list == null )
             {
                 return false;
             }
 
-            foreach (Config l in list)
+            foreach ( Config l in list )
             {
-                urls.Add(l.Index, l.Value);
+                urls.Add( l.Index, l.Value );
             }
 
             return true;
         }
 
-        private void SaveDictionary()
+        private void SaveDictionary ()
         {
             List<Config> list = new List<Config>();
 
-            foreach (KeyValuePair<string, string> Peeve in cmds)
+            foreach ( KeyValuePair<string, string> Peeve in cmds )
             {
-                list.Add(new Config(Peeve.Key, Peeve.Value));
+                list.Add( new Config( Peeve.Key, Peeve.Value ) );
             }
 
-            Host.PluginConfigManager.Save(list, "sblinkcommands", "cmds.cfg");
+            Host.PluginConfigManager.Save( list, "sblinkcommands", "cmds.cfg" );
 
             list = new List<Config>();
 
-            foreach (KeyValuePair<string, string> Peeve in urls)
+            foreach ( KeyValuePair<string, string> Peeve in urls )
             {
-                list.Add(new Config(Peeve.Key, Peeve.Value));
+                list.Add( new Config( Peeve.Key, Peeve.Value ) );
             }
 
-            Host.PluginConfigManager.Save(list, "sblinkcommands", "urls.cfg");
+            Host.PluginConfigManager.Save( list, "sblinkcommands", "urls.cfg" );
         }
 
-        void Host_eventPluginChannelCommandReceived(string name, string channel, string command, string[] parameters)
+        void Host_eventPluginChannelCommandReceived ( string name, string channel, string command, string[] parameters )
         {
-            switch (command)
+            switch ( command )
             {
                 case "link":
-                    if (Host.PluginUserManager.IsOperator(name, channel))
+                    if ( Host.PluginUserManager.IsOperator( name, channel ) )
                     {
-                        if (parameters.Length == 2)
+                        if ( parameters.Length == 2 )
                         {
-                            if (!cmds.ContainsKey(parameters[0]))
+                            if ( !cmds.ContainsKey( parameters[0] ) )
                             {
-                                cmds.Add(parameters[0], parameters[1]);
+                                cmds.Add( parameters[0], parameters[1] );
                                 SaveDictionary();
-                                Host.PluginResponse(channel, String.Format("'!{0}' has been added. To unlink: !unlink {0}", parameters[0]));
+                                Host.PluginResponse( channel, String.Format( "'!{0}' has been added. To unlink: !unlink {0}", parameters[0] ) );
                             }
                             else
                             {
-                                Host.PluginResponse(channel, String.Format("{0} is already linked!", parameters[0]));
+                                Host.PluginResponse( channel, String.Format( "{0} is already linked!", parameters[0] ) );
                             }
                         }
                         else
                         {
-                            Host.PluginResponse(channel, "Usage: !link [cmd] [msg]");
+                            Host.PluginResponse( channel, "Usage: !link [cmd] [msg]" );
                         }
                     }
                     else
                     {
-                        Host.PluginResponse(channel, "Only @'s can do that");
+                        Host.PluginResponse( channel, "Only @'s can do that" );
                     }
 
                     break;
 
                 case "fetchlink":
-                    if (Host.PluginUserManager.IsOperator(name, channel))
+                    if ( Host.PluginUserManager.IsOperator( name, channel ) )
                     {
-                        if (parameters.Length == 2)
+                        if ( parameters.Length == 2 )
                         {
-                            if (!urls.ContainsKey(parameters[0]))
+                            if ( !urls.ContainsKey( parameters[0] ) )
                             {
-                                urls.Add(parameters[0], parameters[1]);
+                                urls.Add( parameters[0], parameters[1] );
                                 SaveDictionary();
-                                Host.PluginResponse(channel, String.Format("'!{0}' has been added. To unlink: !unlink {0}", parameters[0]));
+                                Host.PluginResponse( channel, String.Format( "'!{0}' has been added. To unlink: !unlink {0}", parameters[0] ) );
                             }
                             else
                             {
-                                Host.PluginResponse(channel, String.Format("{0} is already linked!", parameters[0]));
+                                Host.PluginResponse( channel, String.Format( "{0} is already linked!", parameters[0] ) );
                             }
                         }
                         else
                         {
-                            Host.PluginResponse(channel, "Usage: !fetchlink [cmd] [url]");
+                            Host.PluginResponse( channel, "Usage: !fetchlink [cmd] [url]" );
                         }
                     }
                     else
                     {
-                        Host.PluginResponse(channel, "Only @'s can do that");
+                        Host.PluginResponse( channel, "Only @'s can do that" );
                     }
 
                     break;
 
                 case "unlink":
-                    if (Host.PluginUserManager.IsOperator(name, channel))
+                    if ( Host.PluginUserManager.IsOperator( name, channel ) )
                     {
-                        if (parameters.Length == 1)
+                        if ( parameters.Length == 1 )
                         {
-                            if (cmds.ContainsKey(parameters[0]))
+                            if ( cmds.ContainsKey( parameters[0] ) )
                             {
-                                cmds.Remove(parameters[0]);
+                                cmds.Remove( parameters[0] );
                                 SaveDictionary();
-                                Host.PluginResponse(channel, "Removed!");
+                                Host.PluginResponse( channel, "Removed!" );
                             }
-                            else if (urls.ContainsKey(parameters[0]))
+                            else if ( urls.ContainsKey( parameters[0] ) )
                             {
-                                urls.Remove(parameters[0]);
+                                urls.Remove( parameters[0] );
                                 SaveDictionary();
-                                Host.PluginResponse(channel, "Removed!");
+                                Host.PluginResponse( channel, "Removed!" );
                             }
                             else
                             {
-                                Host.PluginResponse(channel, String.Format("{0} doesn't exist!", parameters[0]));
+                                Host.PluginResponse( channel, String.Format( "{0} doesn't exist!", parameters[0] ) );
                             }
                         }
                         else
                         {
-                            Host.PluginResponse(channel, "Usage: !unlink [cmd]");
+                            Host.PluginResponse( channel, "Usage: !unlink [cmd]" );
                         }
                     }
                     else
                     {
-                        Host.PluginResponse(channel, "Only @'s can do that");
+                        Host.PluginResponse( channel, "Only @'s can do that" );
                     }
                     break;
                 default:
-                    if (cmds.ContainsKey(command))
+                    if ( cmds.ContainsKey( command ) )
                     {
-                        Host.PluginResponse(channel, cmds[command]);
+                        Host.PluginResponse( channel, cmds[command] );
                     }
 
-                    if (urls.ContainsKey(command))
+                    if ( urls.ContainsKey( command ) )
                     {
                         try
                         {
                             WebClient x = new WebClient();
-                            string source = x.DownloadString(new Uri(urls[command]));
-                            Host.PluginResponse(channel, source);
+                            string source = x.DownloadString( new Uri( urls[command] ) );
+                            Host.PluginResponse( channel, source );
                         }
                         catch
                         {
-                            Host.PluginResponse(channel, String.Format("The URL '{0}' is invalid or has timed out.", urls[command]));
+                            Host.PluginResponse( channel, String.Format( "The URL '{0}' is invalid or has timed out.", urls[command] ) );
                         }
                     }
                     break;
